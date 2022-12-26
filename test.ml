2b69b2c67a0e6a96ae7ce6587444455a1dd0b50d
David Hanley <dhanley@groupon.com>
2022-12-26 15:51:36 -0700

    test tweaks

--- test.ml
+++ test.ml
@@ -6,15 +6,13 @@ let test_split() =
   let e n = List.nth ss n in
   assert ((List.length ss) = 4)
 
-
-
 let ar = { name = "dave" ; sex = M ; age = Some 49 }
 
-let parsed = line_to_athlete_row ",dave hanley,49,M,silly"
-                                 (* "1,WAI CHING SOH,,M,10:44:00 AM" *)
+let parsed = line_to_athlete_row ",david hanley,49,M,silly"
+
 let test1 () =
   match parsed with
-    | Some(pr) -> assert (pr.name = "dave hanley");
+    | Some(pr) -> assert (pr.name = "david hanley");
                   assert (pr.sex = M);
                   assert (pr.age = Some 49)
     | None -> assert false
