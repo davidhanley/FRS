0eb85e12d7a38ef5528278122e8e4a9e660a84c6
david hanley <davidhanley@davids-MacBook-Air.local>
2025-03-28 18:58:17 -0700

    code, data

--- parse-runsignup.py
+++ parse-runsignup.py
@@ -4,14 +4,14 @@ lines = open(sys.argv[1]).readlines()
 lines = [ line.strip() for line in lines ]
 
 for i in range(len(lines)):
-  line = lines[i]
-  line = line.split("\t")
+  linestr = lines[i]
+  line = linestr.split("\t")
   #print(line)
-  if len(line)>=4:
+  if ":" in linestr:
     name = lines[i-2]+" "+lines[i-1]
-    gender = line[0]
-    age = line[2]
-    time = line[3]
+    gender = line[2]
+    age = line[3]
+    time = line[0]
 
     print(",".join(["",name,age,gender,time]))
 
